e3f8ad0d4eb9f95af9508b2d5c9001ecf87dee97
prova <st10517@iismarconipieralisi.it>
2025-03-16 13:05:45 +0100

    Versione 0.00

---
+++ app/src/main/java/com/example/verifica_tpsit_luciano_arsene/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.verifica_tpsit_luciano_arsene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
